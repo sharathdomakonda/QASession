f9bcb693523c34803537fbde862b5f574d45e111
sharathdomakonda <sharath.domakonda@campsystems.com>
2018-03-16 16:05:00 +0530

    fixed the logivfor 16 th program

--- 16. LeapYear/Program.cs
+++ 16. LeapYear/Program.cs
@@ -16,15 +16,20 @@ namespace _16.LeapYear
 
             int year=Convert.ToInt32(Console.ReadLine());
 
-            if (year % 4 == 0 && year % 100 == 0)
+            if ( year % 100 == 0)
             {
-                Console.WriteLine("The year is a leap year");
+                Console.WriteLine("The year is a not aleap year");
             }
 
 
-            else
+            else if(year%100==0 && year%400==0)
             {
-                Console.WriteLine("The year is not a leap year");
+                Console.WriteLine("The year is  a leap year");
+            }
+
+            else if(year%4==0)
+            {
+                Console.WriteLine("The year is  a leap year");
             }
 
 

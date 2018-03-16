f9b6b61f29b342ee4d9ed335f5dca9c697bf9d65
sharathdomakonda <sharath.domakonda@campsystems.com>
2018-03-16 16:57:15 +0530

    18th program

---
+++ 18.TriangleFormation/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18.TriangleFormation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("To verify whether the given angles can form a  triangle");
+
+            Console.WriteLine("Enter the angles of a triangle");
+
+            int angle1 = Convert.ToInt32(Console.ReadLine());
+            int angle2 = Convert.ToInt32(Console.ReadLine());
+            int angle3 = Convert.ToInt32(Console.ReadLine());
+
+            if (angle1+angle2+angle3==180)
+
+            {
+                Console.WriteLine("The given angles form a triangle");
+            }
+
+            else
+            {
+                Console.WriteLine("The given angles do not form a triangle");
+            }
+
+            Console.ReadLine();
+        }
+
+
+    }
+}

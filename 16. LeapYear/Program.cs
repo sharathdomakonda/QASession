0c92df8a0ef63571676219ebd684e4ae19da12cb
sharathdomakonda <sharath.domakonda@campsystems.com>
2018-03-16 15:57:27 +0530

    including 16th program

---
+++ 16. LeapYear/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16.LeapYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("To find whether an year is leap year or not");
+            Console.WriteLine("Enter the year");
+
+            int year=Convert.ToInt32(Console.ReadLine());
+
+            if (year % 4 == 0 && year % 100 == 0)
+            {
+                Console.WriteLine("The year is a leap year");
+            }
+
+
+            else
+            {
+                Console.WriteLine("The year is not a leap year");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
